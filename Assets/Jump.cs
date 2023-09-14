21589113148010befd97a19bfb2e10ac4150fdf1
ckusupati <ack160130@utdallas.edu>
2023-09-14 05:16:29 -0500

    jump mechanic and sandbox

---
+++ Assets/Jump.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+public class Jump : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float jumpAmount = 35;
+    public float gravityScale = 10;
+    public float fallingGravityScale = 40;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+        }
+        if (rb.velocity.y >= 0)
+        {
+            rb.gravityScale = gravityScale;
+        }
+        else if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = fallingGravityScale;
+        }
+    }
+}
