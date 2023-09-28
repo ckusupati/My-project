5e932c28acf053813f19f8c6bf0c4d020e96d83a
ckusupati <ack160130@utdallas.edu>
2023-09-28 10:56:00 -0500

    Small Tweaks
    small updates

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -89,7 +89,7 @@ public class Jump : MonoBehaviour
 
     void speedCheck()
     {
-        if (UnityEngine.Mathf.Pow(rb.velocity.x, 2) < 0.001 && UnityEngine.Mathf.Pow(rb.velocity.y, 2) < 0.001)
+        if (UnityEngine.Mathf.Pow(rb.velocity.x, 2) < 0.001 && UnityEngine.Mathf.Pow(rb.velocity.y, 2) < 0.001 && boostTimer <= 0)
         {
             SceneManager.LoadScene(Respawn);
         }
